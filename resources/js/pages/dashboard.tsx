a9cf5c89f8fd55a5fa61c3937f24393ae646176b
Korthose <Leo.Korthaus9@gmail.com>
2026-01-27 12:58:46 +0100

    Created a initial wordle form (grid + keyboard)

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { Head } from '@inertiajs/react';
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import WordleForm from '@/components/wordle-form';
 import AppLayout from '@/layouts/app-layout';
 
 export default function Dashboard() {
@@ -7,7 +7,7 @@ export default function Dashboard() {
         <AppLayout>
             <Head title="Daily Wordle" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-
+                <WordleForm></WordleForm>
             </div>
         </AppLayout>
     );
