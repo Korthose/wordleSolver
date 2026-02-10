c2b77ed1629455ba64e355af29d5859fd4dc2e57
Korthose <Leo.Korthaus9@gmail.com>
2026-02-10 12:10:51 +0100

    Created a seeder for the word database

--- database/seeders/WordSeeder.php
+++ database/seeders/WordSeeder.php
@@ -2,16 +2,53 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class WordSeeder extends Seeder
 {
+
+    // Gets the txt file
+    private function getTxt(): string
+    {
+        $path = resource_path('valid-wordle-words.txt');
+
+        return File::get($path);
+    }
+
+    // Formats txt file
+    private function formatTxt(): array
+    {
+        $file = $this->getTxt();
+
+        // generally use PHP_EOL for new line
+        return explode(PHP_EOL, $file);
+    }
+
     /**
-     * Run the database seeds.
+     * Deletes the last entry, as it most likely empty
+     * due to formatting issues with the txt fetching
      */
+    private function deleteLastEntry()
+    {
+        return DB::table('words')
+            ->orderByDesc('id')
+            ->limit(1)
+            ->delete();
+    }
+
+
+     // Run the database seeds.
     public function run(): void
     {
-        //
+        $words = $this->formatTxt();
+        foreach ($words as $word) {
+            DB::table('words')->insert([
+                'word' => $word
+            ]);
+        }
+
+        $this->deleteLastEntry();
     }
 }
