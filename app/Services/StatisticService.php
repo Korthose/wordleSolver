d9f5585f7bda4b198048a9b9cf3da9695c325e72
Korthose <Leo.Korthaus9@gmail.com>
2026-02-12 15:23:01 +0100

    Added method to calculate the position amount of a letter in the entire word db

--- app/Services/StatisticService.php
+++ app/Services/StatisticService.php
@@ -2,6 +2,46 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
 class StatisticService
 {
+    private $sum;
+
+    private $words;
+
+    private $letter;
+
+    private $config;
+
+    private $alphabet;
+
+    /**
+     * @param  $sum
+     */
+    public function __construct()
+    {
+        $this->sum = DB::table('words')->count();
+        $this->words = json_decode(DB::table('words')->select('word')->get(), true);
+        $this->alphabet = Config::collection('alphabet_template')->toArray();
+    }
+
+    public function findCharacterPositions()
+    {
+        //
+    }
+
+    public function createStatistic()
+    {
+        foreach ($this->words as $word) {
+            foreach (str_split(implode($word)) as $i => $l) {
+                $this->alphabet[$l][$i] += 1;
+            }
+        }
+
+        $content = '<?php'.PHP_EOL.PHP_EOL.'return '.var_export($this->alphabet, true).';';
+        File::put(storage_path('alphabet_template.php'), $content);
+    }
 }
