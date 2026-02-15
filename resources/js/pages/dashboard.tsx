d378177f1fbf8e89072757774895f9ba9c1f0cbd
Korthose <Leo.Korthaus9@gmail.com>
2026-02-15 13:56:19 +0100

    Changed WordleForm tag to short tag for better readability

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { Head } from '@inertiajs/react';
-import WordleForm from '@/components/wordle-form';
+import { WordleForm } from '@/components/wordle-form';
 import AppLayout from '@/layouts/app-layout';
 
 export default function Dashboard() {
@@ -7,7 +7,7 @@ export default function Dashboard() {
         <AppLayout>
             <Head title="Daily Wordle" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <WordleForm></WordleForm>
+                <WordleForm/>
             </div>
         </AppLayout>
     );
